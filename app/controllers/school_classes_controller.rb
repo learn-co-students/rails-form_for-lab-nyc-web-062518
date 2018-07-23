375fce8469326e0669ce13bd16b902b7609d7073
grandballoon <luke.sovern.conley@gmail.com>
2018-07-23 13:37:51 -0400

    form_for lab

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,37 @@
+class SchoolClassesController < ApplicationController
+
+  def new
+    @school_class = SchoolClass.new
+    render :new
+  end
+
+  def create
+    @school_class = SchoolClass.create(school_class_params)
+    redirect_to @school_class
+  end
+
+  def show
+    @school_class = SchoolClass.find_by(id: params[:id])
+  end
+
+  def edit
+    @school_class = SchoolClass.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @school_class = SchoolClass.find_by(id: params[:id])
+    @school_class.update(school_class_params)
+    redirect_to @school_class
+  end
+
+
+
+
+
+  private
+
+  def school_class_params
+    params.require(:school_class).permit(:title, :room_number)
+  end
+end
